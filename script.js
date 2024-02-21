8d664c5cc5cb561693eb61bd38d81480798704ce
mhkmhsh <mahekmahesh12@gmail.com>
2024-02-21 09:32:23 -0500

    save user ent

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-let userName = document.getElementById('userName')
-let scoreBoard = parseInt(document.getElementById('scoreBoard').value)
+
+//let scoreBoard = parseInt(document.getElementById('scoreBoard').value)
 
 
 let challengerName = `${userName}`
-let nameDisplay = document.getElementById('nameDisplay')
+let nameDisplayed = document.getElementById('nameDisplay')
+
+function nameDisplay() {
+    let userName = document.getElementById('userName')
+    if (typeof userName === 'string') {
+        nameDisplay.textContent = userName
+    }
+    else{
+        prompt("PLEASE ENTER USERNAME")
+    }
+}
