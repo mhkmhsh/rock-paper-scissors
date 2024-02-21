d2e7f06a1659ed50e3e96b6a8368123ded3ffce5
mhkmhsh <mahekmahesh12@gmail.com>
2024-02-21 09:12:54 -0500

    save

---
+++ script.js
@@ -0,0 +1,6 @@
+let userName = document.getElementById('userName')
+let scoreBoard = parseInt(document.getElementById('scoreBoard').value)
+
+
+let challengerName = `${userName}`
+let nameDisplay = document.getElementById('nameDisplay')
