a82a08ac891168af87e623f6f98bc91929481b94
mhkmhsh <mahekmahesh12@gmail.com>
2024-02-23 11:40:01 -0500

    lkasfghvlorehgbWRIVBGO;lerhg;OERGHLAKJERGHKADFHBLOARHN;eorihgb;ow/rlgbWLEGH;lwrgh;LRSGB

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-
-//let scoreBoard = parseInt(document.getElementById('scoreBoard').value)
-
 let nameDisplayed = prompt('please enter a username ( ex: rock master) ')
 
 let userName = `${nameDisplayed}`
 
-nameDisplay.innerHTML=`<h1>${userName}</h1>`
+nameDisplay.innerHTML = `<h1>${userName}</h1>`
+
+const getComputerChoice = () => {
+    const randomNumber = Math.floor(Math.random() * 3);
+    switch (randomNumber) {
+      case 0:
+        return 'rock';
+      case 1:
+        return 'paper';
+      case 2:
+        return 'scissors';
+    }
+  };
+
+  
+
+  const getUserChoice = userInput => {
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors' || userInput === 'bomb') { 
+        return userInput;
+    } else {
+      console.log('Error!');
+    }
+  }
